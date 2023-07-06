faaec336d3d715b45696a0cb292d11bcc88cc55a
Bertrand31 <bertrandjun@gmail.com>
2023-07-06 15:36:46 +0200

    Add indent-blankline to neovim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,12 @@
+vim.cmd [[highlight IndentBlanklineIndent1 guibg=#292d3e gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guibg=#262836 gui=nocombine]]
+
 return require('packer').startup(function(use)
 
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Scala
   use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }})
 
   -- Completion
@@ -18,6 +22,22 @@ return require('packer').startup(function(use)
 
   use {
     "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  -- Vertical indentation lines
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function () require("indent_blankline").setup {
+      char = "",
+      char_highlight_list = {
+          "IndentBlanklineIndent1",
+          "IndentBlanklineIndent2",
+      },
+      space_char_highlight_list = {
+          "IndentBlanklineIndent1",
+          "IndentBlanklineIndent2",
+      },
+      show_trailing_blankline_indent = false,
+    } end
   }
 end)
