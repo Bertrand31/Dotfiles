e4d5ad2510ce393e4ec2c1fe81bb3a7f45292022
Bertrand31 <bertrandjun@gmail.com>
2023-09-07 18:16:29 +0200

    Improve telescope layouts and bindings

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,9 +1,19 @@
 require('telescope').setup {
   defaults = {
     layout_config = {
-      preview_cutoff = 140,
-    }
-  }
+      preview_cutoff = 150,
+      width = 0.90,                                                                                               
+    },
+  },
+  pickers = {
+    find_files = {
+      theme = "ivy",
+      -- disable preview somehow?
+    },
+    live_grep = {
+      theme = "dropdown",
+    },
+  },
 }
 
 local builtin = require('telescope.builtin')
@@ -11,4 +21,7 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 vim.keymap.set('n', '<C-o>', builtin.buffers, {})
 vim.keymap.set('n', '<C-i>', builtin.help_tags, {})
+vim.keymap.set('n', '<C-d>', builtin.diagnostics, {})
+vim.keymap.set('n', '<C-h>', builtin.git_branches, {})
+vim.keymap.set('n', '<C-s>', builtin.lsp_workspace_symbols, {})
 
