acbbfa05da410c27037b93a518a990e54c2e01b2
Bertrand31 <bertrandjun@gmail.com>
2023-10-16 15:42:00 +0200

    Improve treesitter conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,4 +23,14 @@ return {
       })
     end
   end,
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      -- for syntax-highlight, instead of regular expressions, use tree-sitter:
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
 }
