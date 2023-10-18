24570280d02327f9f1f44e109f43d8c9888c2d97
Bertrand31 <bertrandjun@gmail.com>
2023-10-18 14:25:07 +0200

    Add better signs for diagnostics

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -62,6 +62,11 @@ return {
         on_attach = lsp_on_attach,
       })
     end
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " } 
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
+    end
   end,
   ft = {
     'javascript', 'javascriptreact', 'typescript', 'typescriptreact',
