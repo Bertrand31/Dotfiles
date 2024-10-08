961d7ec6fd04be4fe0f2d5849afe905c0ace73c3
Bertrand31 <bertrandjun@gmail.com>
2024-10-08 15:51:56 +0200

    Add more settings to Metals, fix type lookup

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -37,6 +37,9 @@ return {
 		local metals_config = metals.bare_config()
 		metals_config.settings = {
 			showImplicitArguments = true,
+      showImplicitConversionsAndClasses = true,
+      showInferredType = true,
+      superMethodLensesEnabled = true,
 		}
 		metals_config.init_options.statusBarProvider = "on"
 		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
