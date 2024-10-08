f0756e6693600d826607b1b6eea3d2e3624e1238
Bertrand31 <bertrandjun@gmail.com>
2024-10-08 15:51:41 +0200

    Rename obsolete tsserver

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -57,7 +57,7 @@ return {
     local lspconfig = require('lspconfig')
     -- Required for TS:  npm install -g typescript typescript-language-server vscode-langservers-extracte
     -- Required for rust_analyzer: https://stackoverflow.com/a/77453276
-    for _, server in pairs({ 'eslint', 'tsserver', 'rust_analyzer', 'hls', 'pyright' }) do
+    for _, server in pairs({ 'eslint', 'ts_ls', 'rust_analyzer', 'hls', 'pyright' }) do
       lspconfig[server].setup({
         capabilities = lsp_capabilities,
         on_attach = lsp_on_attach,
